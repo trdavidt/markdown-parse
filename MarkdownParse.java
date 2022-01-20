34dd3d2e6dd896a40ac327b78fb209a5ba252872
David Tran <tr.davidt@gmail.com>
2022-01-20 09:29:20 -0800

    Fix infinite loop for file with [] before link

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -4,8 +4,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
-
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
