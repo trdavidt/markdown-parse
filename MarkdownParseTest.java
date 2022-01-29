511e6407c6f43db492663f9735ffbc9a259b0b1e
David Tran <tr.davidt@gmail.com>
2022-01-28 16:08:55 -0800

    Add new tests with failure-inducing inputs (4, 5, 7, 8)

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -4,8 +4,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
+    // Helper method to get contents of file as String given path as String
+    public static String getContents(String filePath) throws IOException {
+        Path fileName = Path.of(filePath);
+        String contents = Files.readString(fileName);
+        return contents;
+    }
+
     @Test
     public void testDefaultLinkMethod() throws IOException {
         Path fileName = Path.of("test-file.md");
@@ -29,5 +37,42 @@ public class MarkdownParseTest {
         Path fileName = Path.of("markdown-test-three.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of("thiswillbreak.com", "thisisover.com", "peasy.html"), MarkdownParse.getLinks(contents));
+    }
+
+    // Tests from Joe's new repo
+    @Test
+    public void joesNewTestTwo() throws IOException {
+        assertEquals(List.of("https://something.com", "some-page.html"), 
+            MarkdownParse.getLinks(getContents("test-file2.md")));
+    }
+    @Test
+    public void joesNewTestThree() throws IOException {
+        assertEquals(List.of(""), 
+            MarkdownParse.getLinks(getContents("test-file3.md")));
+    }
+    @Test
+    public void joesNewTestFour() throws IOException {
+        assertEquals(new ArrayList<String>(), 
+            MarkdownParse.getLinks(getContents("test-file4.md")));
+    }
+    @Test
+    public void joesNewTestFive() throws IOException {
+        assertEquals(List.of("page.com"), 
+            MarkdownParse.getLinks(getContents("test-file5.md")));
+    }
+    @Test
+    public void joesNewTestSix() throws IOException {
+        assertEquals(List.of("page.com"), 
+            MarkdownParse.getLinks(getContents("test-file6.md")));
+    }
+    @Test
+    public void joesNewTestSeven() throws IOException {
+        assertEquals(new ArrayList<String>(), 
+            MarkdownParse.getLinks(getContents("test-file7.md")));
+    }
+    @Test
+    public void joesNewTestEight() throws IOException {
+        assertEquals(new ArrayList<String>(), 
+            MarkdownParse.getLinks(getContents("test-file8.md")));
     }
 }
