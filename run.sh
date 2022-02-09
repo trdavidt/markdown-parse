65e8c00be14d7afc4b0b6bd8659e3901d15a69b5
trdavidt <tr.davidt@gmail.com>
2022-02-09 15:04:41 -0800

    Add Makefile to run tests

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
-javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
+set -e
 
-java -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+CLASSPATH=".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar"
+
+javac -cp $CLASSPATH MarkdownParseTest.java
+
+# if [ $? != 0 ]
+# then 
+# 	echo "Exiting early"
+# 	exit 1
+# fi
+
+echo $?
+
+
+java -cp $CLASSPATH org.junit.runner.JUnitCore MarkdownParseTest
