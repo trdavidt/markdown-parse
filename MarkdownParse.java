ad9d52c9aa090855726b799a546ed1cd66a61a0d
trdavidt <tr.davidt@gmail.com>
2022-02-11 16:19:00 -0800

    Test commit from ieng6

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -4,6 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+// Parse all links in Markdown files
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
